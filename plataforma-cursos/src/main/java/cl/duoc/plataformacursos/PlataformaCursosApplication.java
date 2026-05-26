d11872a9da97fd0cb92653b40660f08a32760181
Rodrigo1223 <rod.olavarria@duocuc.cl>
2026-05-25 22:46:01 -0400

    Add files via upload

---
+++ plataforma-cursos/src/main/java/cl/duoc/plataformacursos/PlataformaCursosApplication.java
@@ -0,0 +1,11 @@
+package cl.duoc.plataformacursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaCursosApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformaCursosApplication.class, args);
+    }
+}
